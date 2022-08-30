d6fccfe412acfb42b253148affe7decdd0341843
USER <USER@DESKTOP-7HAGNTD>
2022-08-30 18:47:23 +0530

    first commit

---
+++ mavanfire/src/main/java/basics/intro.java
@@ -0,0 +1,14 @@
+package basics;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class intro {
+@Test
+public void t1()
+{
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\USER\\Downloads\\chromedriver.exe");
+	ChromeDriver ob =new ChromeDriver();
+	ob.get("http://www.google.com");
+}
+}
